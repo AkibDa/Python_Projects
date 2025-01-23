0f0e6dda5c36b0355fd2684b0be031701363d821
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-01-23 11:27:40 +0530

    basic calc

---
+++ First/second.py
@@ -0,0 +1,11 @@
+a=5;
+b=7;
+
+ans1 = a+b;
+print("Addition of",a,"and",b,"is equal to",ans1);
+ans2 = a-b;
+print("Substraction of",a,"and",b,"is equal to",ans2);
+ans3 = a*b;
+print("Multiplication of",a,"and",b,"is equal to",ans3);
+ans4 = a/b;
+print("Division of",a,"and",b,"is equal to",ans4);
