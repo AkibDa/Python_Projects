511e04d3c236e442e87ca0bc7b045d1557ba2f08
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-01-24 19:44:07 +0530

    Printing a poem

--- Basic/first.py
+++ Basic/first.py
@@ -1,1 +1,2 @@
+#Printing a poem
 print("Beneath the sky so vast and blue,\nThe world awakens, fresh and new.\nA gentle breeze, the sun's soft glow,\nA timeless dance, a tranquil show.\nThe whispers of the leaves that sway,\nGuide hearts that wander far away.\nIn every moment, joy can bloom,\nA spark of light in every room.");
