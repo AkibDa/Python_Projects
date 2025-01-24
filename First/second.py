fb94c08424ee16a8ec3d4651ec4ca83911fe65f2
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-01-24 11:49:42 +0530

    Improved the calc

--- First/second.py
+++ First/second.py
@@ -1,11 +1,23 @@
 a=int(input("Enter first number: "));
 b=int(input("Enter second number: "));
+c=input("Choose a math operationc(+,-,*,/): ")
 
-ans1 = a+b;
-print("Addition of",a,"and",b,"is equal to",ans1);
-ans2 = a-b;
-print("Substraction of",a,"and",b,"is equal to",ans2);
-ans3 = a*b;
-print("Multiplication of",a,"and",b,"is equal to",ans3);
-ans4 = a/b;
-print("Division of",a,"and",b,"is equal to",ans4);
+# ans1 = a+b;
+# print("Addition of",a,"and",b,"is equal to",ans1);
+# ans2 = a-b;
+# print("Substraction of",a,"and",b,"is equal to",ans2);
+# ans3 = a*b;
+# print("Multiplication of",a,"and",b,"is equal to",ans3);
+# ans4 = a/b;
+# print("Division of",a,"and",b,"is equal to",ans4);
+
+match c:
+  case '+':
+    print("Addition of",a,"and",b,"is equal to",a+b);
+  case '-':
+    print("Substraction of",a,"and",b,"is equal to",a-b);
+  case '*':
+    print("Multiplication of",a,"and",b,"is equal to",a*b);
+  case '/':
+    print("Division of",a,"and",b,"is equal to",a/b);
+
