869be2a8180b0871daf387aff59b41474d80ae67
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-01-24 11:26:33 +0530

    understanding if-elif-else statements

---
+++ First/four.py
@@ -0,0 +1,31 @@
+a = int(input("Enter your age: "));
+print("Your age is:",a);
+
+if(a>18):
+  print("You can drive");
+else:
+  print("You cannot drive");
+
+# print(a>18);
+# print(a<=18);
+# print(a==18);
+# print(a!=18);
+
+applePrice = 10;
+budget = 200;
+
+if (budget - applePrice > 50):
+  print("Alexa, add 1kg Apples to the cart.")
+elif (budget - applePrice > 70):
+  print("Its okay you can buy. ")
+else :
+  print("Alexa, do not add Apples to the cart. ")
+
+num = int(input("Enter any number: "));
+
+if(num > 0):
+  print("Number is Positive");
+elif(num < 0):
+  print("Number is Negative");
+else:
+  print("Number is Zero");
