15c8b68a37bcae37a4f59a2f03757d80dcc500f7
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-01-26 10:36:05 +0530

    Understanding diff type of loops

--- Basic/sixth.py
+++ Basic/sixth.py
@@ -35,3 +35,21 @@ for x in range(5):
 else:
   print("else block in loop")
 print("Out of loop")
+
+fruits = ['apple','banna','mango']
+for index, fruit in enumerate(fruits):
+  print(index, fruit)
+
+marks = [12,56,32,98,12,45,1,4]
+
+# index = 0
+# for mark in marks:
+#   print(mark)
+#   if(index == 3):
+#     print("Harry, awesome!")
+#   index += 1
+
+for index, mark in enumerate(marks):
+  print(mark)
+  if(index == 3):
+    print("Harry, awesome!")
