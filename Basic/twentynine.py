1206e315061508666b94673b5041768a9beffd42
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-01-28 18:43:32 +0530

    Multilevel Inheritance

--- Basic/twentynine.py
+++ Basic/twentynine.py
@@ -4,22 +4,30 @@ class Animal:
     self.name = name
     self.species = species
 
-  def make_sound(self):
-    print("Sound made by the animal")
+  def show_details(self):
+    print(f"Name: {self.name}")
+    print(f"Species: {self.species}")
 
 class Dog(Animal):
   def __init__(self, name, breed):
     Animal.__init__(self, name, species="Dog")
     self.breed = breed
 
-  def make_sound(self):
-    print("Bark!")
+  def show_details(self):
+    Animal.show_details(self)
+    print(f"Breed: {self.breed}")
 
-d = Dog("Dog","Doggerman")
-d.make_sound()
+class GoldenRetriver(Dog):
+  def __init__(self, name, color):
+    Dog.__init__(self, name, breed="Golden Retriver")
+    self.color = color
 
-a = Animal("Dog", "Dog")
-a.make_sound()
+  def show_details(self):
+    Dog.show_details(self)
+    print(f"Color: {self.color}")
+
+d = GoldenRetriver("Tommy","Black")
+d.show_details()
 
 class Employee:
   
