78c071efc987460fcff74a9a0a978150d37985e0
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-01 10:13:07 +0530

    generator and function caching

--- Basic/thirtyfive.py
+++ Basic/thirtyfive.py
@@ -1,3 +1,6 @@
+import functools
+import time
+
 def my_generator():
   for i in range(5):
     yield i
@@ -9,3 +12,29 @@ print(next(gen))
 print(next(gen))
 print(next(gen))
 
+@functools.lru_cache(maxsize=None)
+def fib(n):
+  if n < 2:
+    return n
+  return fib(n-1) + fib(n-2)
+
+print(fib(20))
+
+@functools.lru_cache(maxsize=None)
+def fx(n):
+  time.sleep(5)
+  return n*5
+
+print(fx(20))
+print("done for 20")
+print(fx(2))
+print("done for 2")
+print(fx(6))
+print("done for 6")
+
+print(fx(20))
+print("done for 20")
+print(fx(2))
+print("done for 2")
+print(fx(6))
+print("done for 6")
