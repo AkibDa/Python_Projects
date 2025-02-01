19e2d40fd6f642826d090d93db29554b96cfd8da
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-01 09:58:31 +0530

    News App

--- Basic/thirtyfour.py
+++ Basic/thirtyfour.py
@@ -1,7 +1,7 @@
 import requests
-import creds
+from creds import API_KEY
 
-url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={creds.API_KEY}'
+url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
 
 response = requests.get(url)
 
